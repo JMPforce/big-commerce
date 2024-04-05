6eab3a5eedf58e604a3f94bd72771bd906242aea
Sahbaj <sahbaj.uddin@bglobal.com>
2024-04-05 20:57:30 +0600

    debugging

--- get-order-shipment.php
+++ get-order-shipment.php
@@ -13,7 +13,7 @@ if ($_SERVER["SERVER_NAME"] == "big-commerce.local") {
 
 $connection = db_connection();
 
-if ((!empty($vPayload) && count($vPayload)) > 0 || !empty($vQuery)) {
+if ((!empty($vPayload) && count($vPayload)) > 0) {
     $vParam["api_url"] =  "orders/" . $vPayload["order_id"] . "/shipments";
     $vParam["method"] = "GET";
     unset($vPayload["order_id"]);
@@ -21,21 +21,15 @@ if ((!empty($vPayload) && count($vPayload)) > 0 || !empty($vQuery)) {
 
     $vReturnData = call_big_commerce_api($vParam, "v2");
 
-    if (!isset($vReturnData->data)) {
+    if ($_SERVER["SERVER_NAME"] == "big-commerce.local")
         echo json_encode($vReturnData);
-    } else {
-        if ($_SERVER["SERVER_NAME"] == "big-commerce.local")
-            echo json_encode($vReturnData);
-        else
-            v::$r = vR(200, $vReturnData);
-    }
+    else
+        v::$r = vR(200, $vReturnData);
 } else {
 
-    $data["scope"] = "store/shipment/created";
-
-    $sql = "SELECT * FROM  {$vTable} WHERE scope='store/shipment/created' OR scope='store/shipment/updated' OR scope='store/shipment/deleted' ORDER BY created_at DESC";
+    echo   $sql = "SELECT * FROM  {$vTable} WHERE scope='store/shipment/created' OR scope='store/shipment/updated' OR scope='store/shipment/deleted' ORDER BY created_at DESC";
     $result = select($connection, $sql);
-
+    closeConnection($connection);
     $vReturnData = [];
     foreach ($result as $key => $row) {
         $vReturnData[$key]["scope"] = $row["scope"];
@@ -51,4 +45,3 @@ if ((!empty($vPayload) && count($vPayload)) > 0 || !empty($vQuery)) {
         v::$r = vR(200, $vReturnData);
     }
 }
-closeConnection($connection);
