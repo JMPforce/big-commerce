c5236d971a5ab5532f9d33e5df6d472ab24337f5
Sahbaj <sahbaj.uddin@bglobal.com>
2024-04-23 04:09:45 +0600

    DEBUGG

--- product-lists.php
+++ product-lists.php
@@ -8,15 +8,18 @@ $vQuery = "";
 parse_str($_SERVER['QUERY_STRING'], $vQuery);
 
 $vParam["api_url"] =  "catalog/products";
-// if (isset($vQuery["limit"])) {
-//     $vQueryString .= "?limit=" . $vQuery["limit"];
-// }
-// if (isset($vQuery["page"])) {
-//     $vParam["api_url"] .= "?limit=" . $vQuery["limit"] . "&page=" . $vQuery["page"];
-// }
-if (isset($_SERVER['QUERY_STRING']) && $_SERVER['QUERY_STRING'] != "") {
-    $vParam["api_url"] .= "?" . $_SERVER['QUERY_STRING'];
+if (isset($vQuery["limit"])) {
+    $vQueryString .= "?limit=" . $vQuery["limit"];
 }
+if (isset($vQuery["page"])) {
+    $vParam["api_url"] .= "&page=" . $vQuery["page"];
+}
+if (isset($vQuery["categories:in"])) {
+    $vParam["api_url"] .= "&categories:in=" . $vQuery["categories:in"];
+}
+// if (isset($_SERVER['QUERY_STRING']) && $_SERVER['QUERY_STRING'] != "") {
+//     $vParam["api_url"] .= "?" . $_SERVER['QUERY_STRING'];
+// }
 $vParam["method"] = "GET";
 print_r($vParam);
 $vReturnData = call_big_commerce_api($vParam);
