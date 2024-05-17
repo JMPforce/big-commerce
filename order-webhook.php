1e195a25d38d7235b9647101fb3779baba9bb236
Sahbaj <sahbaj.uddin@bglobal.com>
2024-05-18 04:04:55 +0600

    Shipperinfo stored for each item in PostgreSQl to use by Webhook

--- order-webhook.php
+++ order-webhook.php
@@ -94,7 +94,7 @@ if ($vPayload["data"]["id"] && $vPayload["scope"] = "store/order/created") {
 
                         $vParam["api_url"] =  "labels";
                         $vParam["method"] = "POST";
-                        $reference = "reference-" . $vOrderId . "-" . ($key + 1) . "-" . $row->product_id . "_" . $i;
+                        $reference = "reference-" . $vOrderId . "-" . $row->product_id . "_" . $i;
                         $vParam["body"]["order_id"] = $reference;
                         $vParam["body"]["order_number"] = $reference;
                         $vParam["body"]["return_shipment"] = false;
