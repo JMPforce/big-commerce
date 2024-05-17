0b716713da00ba47d57eb0b16fd0273659c2cb71
Sahbaj <sahbaj.uddin@bglobal.com>
2024-05-18 00:11:50 +0600

    Multiple label creation per order item

--- functions.php
+++ functions.php
@@ -358,3 +358,12 @@ function update_customer_address($vPayload)
 
     return call_big_commerce_api($vParam);
 }
+
+function findIndexByName($array, $name) {
+    foreach ($array as $index => $object) {
+        if ($object->name === $name) {
+            return $index;
+        }
+    }
+    return -1; // Return -1 if the name is not found
+}
