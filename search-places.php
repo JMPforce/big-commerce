6b5151fd456f1caaa55d88a2aaeed221da78ce73
Sahbaj <sahbaj.uddin@bglobal.com>
2024-05-23 08:24:20 +0600

    Place API

--- search-places.php
+++ search-places.php
@@ -24,33 +24,16 @@ if (count($vResponse) > 0) {
     }
 } else {
 
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=Powerscourt%20Golf%20Club&key=AIzaSyAbvzlTuxrYxM63Oyf-JFvXqqaZR08P5ck&region=IE&language=en-IE',
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'GET',
-    ));
-
-    // $response = curl_exec($curl);
-
-    // curl_close($curl);
-    // echo $response;
-
-
 
     $vApiKey = $GLOBALS["vConfig"]["PLACE_API_KEY"];
     $vQuery = urlencode($vPayload["query"]);
-    // http_build_query(["query"=>])
-    $vParam["api_url"] =  "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" . $vQuery . "&key=" . $vApiKey . "&language=en";
-    // $vParam["api_url"] =  "https://maps.googleapis.com/maps/api/place/textsearch/json?query=Powerscourt Golf Club&key=AIzaSyAbvzlTuxrYxM63Oyf-JFvXqqaZR08P5ck&region=IE&language=en-IE";
+
+    $vQueryString = "key=" .  $vApiKey . "&query=" . $vQuery . "&language=en";
+    if ($vPayload["country"]) {
+        $vQueryString .= "&region=" . strtoupper($vPayload["country"]);
+    }
+    $vParam["api_url"] =  "https://maps.googleapis.com/maps/api/place/textsearch/json?" . $vQueryString;
     $vParam["method"] = "GET";
-    // print_r($vParam);
     $vReturnData = call_google_place_api($vParam);
 
     if (!isset($vReturnData->data)) {
